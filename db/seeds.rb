419010c1c9b8e042f48946f832cb3f4c9af2bdc2
Ángel Díaz <diazangel9311@gmail.com>
2021-11-08 18:26:50 -0500

    Implement Message scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create([
+  {
+    content: 'Hello World!'
+  },
+  {
+    content: '¡Hola Mundo!'
+  },
+  {
+    content: 'Olá o Mundo!'
+  },
+  {
+    content: 'Bonjour Monde!'
+  },
+  {
+    content: 'Ciao Mundo!'
+  }
+]);
